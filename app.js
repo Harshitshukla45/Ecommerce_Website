0116c410e17dbaa8a1eca174befd41b799bf4f16
Harshit Shukla <shuklaharshit51@gmail.com>
2023-08-11 00:17:45 +0530

    change done

--- app.js
+++ app.js
@@ -2,24 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const path = require('path'); // Import path module
 require("./conn");
 const Products = require("./pdSchema");
 const DefaultData = require('./defaultData');
 const cors = require("cors");
 const router = require("./router");
 const cookieParser = require("cookie-parser");
-const path = require('path');
 
 app.use(express.json());
-app.use(cookieParser(""));
-app.use("*", cors({
+app.use(cookieParser());
+app.use(cors({
     origin: true,
     credentials: true
 }));
-app.use(router);
-
-const apiRoutes = require('./router.js');
-app.use('/api', apiRoutes);
+app.use('/api', router);
 
 // Serve the static build of your React app
 const reactBuildPath = path.join(__dirname, 'build'); // Point to the build folder
@@ -30,12 +27,9 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(reactBuildPath, 'index.html'));
 });
 
-
-
 const port = process.env.PORT || 8005;
 app.listen(port, function (err) {
-    console.log("server listening at port 8005");
+    console.log("Server listening at port 8005");
 });
 
 DefaultData();
-
